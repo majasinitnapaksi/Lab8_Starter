3dd902562020a19d2d0fc3e51ab077d3e764f1ef
SoohwanJeon <jsh4059@naver.com>
2026-05-26 12:22:47 -0700

    commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Lab8-Starter
+
+
+
+name: Soohwan Jeon
+
+
+## GitHub Pages URL
+
+https://너의아이디.github.io/Lab8_Starter/
+
+## Meaning of Graceful Degradation and Service Workers 
+
+Graceful degradation means a website can still work in a basic way even if some features fail or the internet is weak. Service workers help by saving important files, so users can still open and use parts of the app offline or with a slow connection.
--- sw.js
+++ sw.js
@@ -3,38 +3,52 @@
 
 const CACHE_NAME = 'lab-8-starter';
 
-// Installs the service worker. Feed it some initial URLs to cache
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './assets/scripts/main.js',
+  './assets/styles/main.css',
+
+  'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+];
+
+// Install service worker
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
-      //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      // B6
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
 });
 
-// Activates the service worker
+// Activate service worker
 self.addEventListener('activate', function (event) {
   event.waitUntil(self.clients.claim());
 });
 
-// Intercept fetch requests and cache them
+// Fetch handler
 self.addEventListener('fetch', function (event) {
-  // We added some known URLs to the cache above, but tracking down every
-  // subsequent network request URL and adding it manually would be very taxing.
-  // We will be adding all of the resources not specified in the intiial cache
-  // list to the cache as they come in.
-  /*******************************/
-  // This article from Google will help with this portion. Before asking ANY
-  // questions about this section, read this article.
-  // NOTE: In the article's code REPLACE fetch(event.request.url) with
-  //       fetch(event.request)
-  // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
-  /*******************************/
-  // B7. TODO - Respond to the event by opening the cache using the name we gave
-  //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+  // B7
+  event.respondWith(
+    caches.open(CACHE_NAME).then(async function (cache) {
+      // B8
+      const cachedResponse = await cache.match(event.request);
+
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      const networkResponse = await fetch(event.request);
+
+      cache.put(event.request, networkResponse.clone());
+
+      return networkResponse;
+    })
+  );
 });
